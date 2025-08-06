fb513bc4a9ce91672bc0c9fb642168aa7d56fde7
JWilliams-Keyin <Jack.Williams@keyin.com>
2025-08-06 12:54:24 -0230

    Added navbar

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,13 +2,14 @@ import '../App.css';
 import HomeTable from '../components/hometable';
 import Header from '../components/header';
 import Footer from '../components/footer';
-import '../components/navbar';
+import NavBar from '../components/navbar';
 
 function Home() {
 
     return (
         <>
           <Header/>
+          <NavBar/>
           <HomeTable/>
           <Footer/>
         </>
